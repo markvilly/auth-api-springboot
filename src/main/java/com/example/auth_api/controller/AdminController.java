cbecd3aeb117effa8df2cc48263a62c074b207e6
Mark Paul <markpaulbusiness66@gmail.com>
2026-03-18 14:14:26 +0300

    Add AdminController and UserController with role-based access; initialize roles in data.sql

---
+++ src/main/java/com/example/auth_api/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.example.auth_api.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController{
+
+    @GetMapping("/dashboard")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> dashboard (){
+        return ResponseEntity.ok("Admin Dashboard");
+    }
+    
+
+}
